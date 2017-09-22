16ef976ad66dcbde1a9b40521da0ef6f70a2521b
Sudhanshu <sudhanshubliz@gmail.com>
2017-09-22 09:01:52 +0530

    Add readme file and Integrate Swagger-UI for test API.

--- src/main/java/com/infoapp/EmployeeAppApplication.java
+++ src/main/java/com/infoapp/EmployeeAppApplication.java
@@ -1,12 +1,24 @@
 package com.infoapp;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
 import com.infoapp.entity.Employee;
 import com.infoapp.service.EmployeeInfoService;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 @SpringBootApplication
 public class EmployeeAppApplication {
 
@@ -23,5 +35,27 @@ public class EmployeeAppApplication {
 		employee.setPassword("admin");
  		 
 		employeeInfoService.createmployee(employee);
+		
+		
 	}
+	 private ApiInfo metaData() {
+	        ApiInfo apiInfo = new ApiInfo(
+	                "Spring Boot REST API",
+	                "Spring Boot REST API for Employee APP",
+	                "1.0",
+	                "Terms of service",
+	                new Contact("Sudhanshu Thakur", "https://localhost:8080/", "sudhanshu.thakur@capgemini.com"),
+	               "Apache License Version 2.0",
+	                "https://www.apache.org/licenses/LICENSE-2.0", null);
+	        return apiInfo;
+	    }
+	 
+	    public Docket employeeApi() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .select()
+	                .apis(RequestHandlerSelectors.basePackage("com.infoapp.controller"))
+	                .paths(regex("/employee.*"))
+	                .build()
+	                .apiInfo(metaData());
+	    }
 }
